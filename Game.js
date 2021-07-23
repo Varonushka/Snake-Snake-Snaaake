76d267e67ac15a1f0ff7eb2cdcef7444339984fc
Nika Mitnik <85923970+Varonushka@users.noreply.github.com>
2021-07-23 08:19:49 +0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -23,7 +23,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You lost. Press ok to restart the game')) {
-            window.location = 'index'
+            window.location = './'
         }
         return
     }
