74ea69d367515b5351819ffe2b6d487aa7a67d78
Nika Mitnik <85923970+Varonushka@users.noreply.github.com>
2021-07-23 08:15:19 +0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -23,7 +23,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You lost. Press ok to restart the game')) {
-            window.location = '/Snake-Snake-Snaaake/'
+            window.location = 'https://varonushka.github.io/Snake-Snake-Snaaake/'
         }
         return
     }
